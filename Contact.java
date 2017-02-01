870f94af904cfc706667beda7d451b4cc139f947
Manirathnamp <manirathnam777@gmail.com>
2017-02-01 18:58:47 +0530

    Add files via upload

---
+++ Contact.java
@@ -0,0 +1,20 @@
+package com.full;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Contact {
+	public static void main(String args[]) throws IOException {
+		Names names = new Names();
+		ArrayList<Object> user = new ArrayList<Object>();
+		
+		names.add();
+		Object obj = names.add();
+		Object obj1 = names.add();
+		user.add(obj);
+		user.add(obj1);
+		System.out.println("done");
+		
+	}
+
+}
